61a90f04ad4cc9aa32078ee68418d40210cc5f94
mgogic <Mirko.Gogic@senacor.com>
2017-02-17 13:02:46 +0100

    Defined simple Get index controller maping

--- src/main/java/com/startup/controllers/MainController.java
+++ src/main/java/com/startup/controllers/MainController.java
@@ -1,16 +1,14 @@
 package com.startup.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by mgogic on 16.02.17.
- */
 @RestController
 public class MainController {
 
-  @RequestMapping("/greeting")
-  public String greeting() {
-    return "jessss";
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String index() {
+    return "Hello World !";
   }
 }
