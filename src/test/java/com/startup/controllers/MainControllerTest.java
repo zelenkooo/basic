aead8ca498662afddba1b022b98b00b92e288b6e
mgogic <Mirko.Gogic@senacor.com>
2017-02-17 20:28:27 +0100

    Added static content

--- src/test/java/com/startup/controllers/MainControllerTest.java
+++ src/test/java/com/startup/controllers/MainControllerTest.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,8 +21,29 @@ public class MainControllerTest {
 
   @Test
   public void indexTest() {
-    String body = this.restTemplate.getForObject("/", String.class);
-    assertThat(body).isEqualTo("Hello World !");
+    ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(entity.getHeaders().getContentType())
+            .isEqualTo(MediaType.valueOf("text/plain;charset=UTF-8"));
+    assertThat(entity.getBody()).contains("Hello World !");
+  }
+
+  @Test
+  public void testCss() throws Exception {
+    ResponseEntity<String> entity = this.restTemplate.getForEntity(
+            "/css/style.css", String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(entity.getHeaders().getContentType())
+            .isEqualTo(MediaType.valueOf("text/css"));
+  }
+
+  @Test
+  public void testJs() throws Exception {
+    ResponseEntity<String> entity = this.restTemplate.getForEntity(
+            "/js/init.js", String.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(entity.getHeaders().getContentType())
+            .isEqualTo(MediaType.valueOf("application/javascript"));
   }
 }
 
